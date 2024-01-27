2171ac37a93281c023d4e8aa7931839bbf4d3251
Adrian Aranda <adrienaranda@gmail.com>
2024-01-27 20:14:48 +0100

    Fix stuff I broke

---
+++ the_world_news_backend/Constants.py
@@ -0,0 +1,2 @@
+MAX_ARTICLES = 12
+REDDIT_URL = "https://reddit.com/r/worldnews.json"
---
+++ the_world_news_backend/services/NewsScraperService.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+from the_world_news_backend.Constants import MAX_ARTICLES, REDDIT_URL
+
+
+def fetch_news():
+    try:
+        response = requests.get(REDDIT_URL)
+        site_json = json.loads(response.text)
+        print(site_json)
+
+        post_titles = [
+            {'title': post['data']['title'], 'url': 'https://www.reddit.com' + post['data']['permalink']}
+            for i, post in enumerate(site_json['data']['children'])
+            if i <= MAX_ARTICLES and 'r/WorldNews Live Thread' not in post['data']['title']
+        ]
+    except:
+        post_titles = [
+            {'title': "Sorry for the inconvenience, it seems Reddit is blocking scraping.\nGive it some time or "
+                      "click here to visit WorldNews subreddit!", 'url': 'https://www.reddit.com/r/worldnews'}
+        ]
+
+    return {'post_titles': post_titles}
